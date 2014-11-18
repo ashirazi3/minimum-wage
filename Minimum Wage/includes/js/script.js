8d4fb7555c3f4a26cfbe6a0c5a1d608af348087b
ashirazi3 <ashirazi@gatech.edu>
2014-11-18 17:07:44 -0500

    fixed autofill on clicking a family size

--- Minimum Wage/includes/js/script.js
+++ Minimum Wage/includes/js/script.js
@@ -13,40 +13,59 @@ $(document).ready(function(){
 		}
 	});
 	$('#map').usmap({stateHoverAnimation: 100});
-    $(window).scroll(function(){
-          updateNavScroll();
-    })
-    $('.nav li a').on('click', function() {
-	    $(this).parent().parent().find('.active').removeClass('active');
-	    $(this).parent().addClass('active');
+	$(window).scroll(function(){
+		updateNavScroll();
+	})
+	$('.nav li a').on('click', function() {
+		$(this).parent().parent().find('.active').removeClass('active');
+		$(this).parent().addClass('active');
 	});
 
 	$('.nav li a').on('click', function() {
 		$(this).parent().parent().find('.active').removeClass('active');
-	    $(this).parent().addClass('active');
-	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-    		&& location.hostname == this.hostname) {
-	      	var $target = $(this.hash);
-	      	$target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
-	      	if ($target.length) {
-	        	var targetOffset = $target.offset().top;
-	        	$('html,body').animate({scrollTop: targetOffset}, 1000);
-	       		return false;
-	      	}
-    	}
-	});
-	
+		$(this).parent().addClass('active');
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+			&& location.hostname == this.hostname) {
+			var $target = $(this.hash);
+		$target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+		if ($target.length) {
+			var targetOffset = $target.offset().top;
+			$('html,body').animate({scrollTop: targetOffset}, 1000);
+			return false;
+		}
+	}
+});
+
 });
 
 function updateNavScroll(){
 	if($(window).scrollTop() > $("#expenses").position().top -110){
-	   $('.nav').find('.active').removeClass('active');
-	    $("#navExpenses").addClass('active');
+		$('.nav').find('.active').removeClass('active');
+		$("#navExpenses").addClass('active');
 	}else if($(window).scrollTop() > $("#state").position().top -110){
-	   $('.nav').find('.active').removeClass('active');
-	    $("#navState").addClass('active');
+		$('.nav').find('.active').removeClass('active');
+		$("#navState").addClass('active');
 	}else{
 		$('.nav').find('.active').removeClass('active');
-	    $("#navFamily").addClass('active');
+		$("#navFamily").addClass('active');
 	}
+}
+
+function setFamily(num){
+	if(num==0){
+		$("#housing").val("639");
+		$("#child").val("0");
+		$("#groceries").val("242");
+		$("#medical").val("123");
+		$("#transportation").val("318");
+		$("#other").val("74");
+	}else if(num === 2){
+		$("#housing").val("782");
+		$("#child").val("417");
+		$("#groceries").val("242");
+		$("#medical").val("393");
+		$("#transportation").val("618");
+		$("#other").val("162");
+	}
+
 }
