9f8ec6355f47bc60951ece19dff9f936afcc8d62
ashirazi3 <ashirazi@gatech.edu>
2014-11-18 13:14:36 -0500

    put in some of the form

--- Minimum Wage/includes/js/script.js
+++ Minimum Wage/includes/js/script.js
@@ -1,9 +1,16 @@
 
 $(document).ready(function(){
 	updateNavScroll();
+	var family = 0;
+	var state = null;
+
 	$('#map').usmap({
 		stateStyles: {fill: '#E0E0E0'},
 		stateHoverStyles:{fill: '#203D6C'},
+		click: function(event, data){
+			state = data.name;
+			console.log(state); 
+		}
 	});
 	$('#map').usmap({stateHoverAnimation: 100});
     $(window).scroll(function(){
@@ -16,7 +23,6 @@ $(document).ready(function(){
 });
 
 function updateNavScroll(){
-	console.log("goes to method");
 	if($(window).scrollTop() > $("#expenses").position().top -110){
 	   $('.nav').find('.active').removeClass('active');
 	    $("#navExpenses").addClass('active');
