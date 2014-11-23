3c87498e575d831a8110a2af61598e80d1b94b1d
codyt321 <cjt@wrek.org>
2014-11-23 15:36:14 -0500

    Dollar now working with calculate button
    Needs a little formatting and more contextual info.
    Most of script.js removed, so only copy and paste the needed sections
    if you want to add to your branch.

--- Minimum Wage/includes/js/script.js
+++ Minimum Wage/includes/js/script.js
@@ -1,43 +1,75 @@
 
 $(document).ready(function(){
-	var wage = null;
-	var total = 0;
+	 wage = 7.25;
+    total = 0;
 
 
 	
 
-});
+    });
+
+	$("#calculate").on('click', function(){
+		total = parseInt(wage)*2040;
+	$("form#expenseForm input[type=number]").each(function(){
+		if($(this).val()!==''){
+				total = parseInt(total) -     
+                        parseInt($(this).val())*parseInt($("#"+$(this).attr('id').concat("rate")).val());
+            console.log("hey");
+        }
+	});
+	if(total<0){
+		$("#number").text('-$' + Math.abs(total));
+
+	}else{
+		$("#number").text('$' + total);
+        
+	}
+
+	if(total>0){
+		$("#number").css("color", "green");
+	}else{
+		$("#number").css("color", "red");
+	}
+    var img = document.getElementById('image');
+    var starting=parseInt(wage)*2040;
+    var budgetLeftOver = calcPerc(starting,total)/100;
+    var clipVal = image.width*budgetLeftOver;
+    console.log("clipVal is: " + clipVal);
+    img.style.clip = getClipVal(clipVal);
+        
+	});
 
 
 
-var img = document.getElementById('image');
-var sldH = document.getElementById('slider-h');
+//var img = document.getElementById('image');
+//var sldH = document.getElementById('slider-h');
 //var sldV = document.getElementById('slider-v');
 
 // attach change handlers to the sliders
-sldH.addEventListener('change', changeHandler);
+//sldH.addEventListener('change', changeHandler);
 //sldV.addEventListener('change', changeHandler);
 
-function changeHandler(e) {
-    
-    var isHorizontal = e.srcElement.id == 'slider-h';
-        
-    // get the sliders values
-    var valH = sldH.value;
-    //var valV = sldV.value;
+//function changeHandler(e) {
+//    
+//    var isHorizontal = e.srcElement.id == 'slider-h';
+//        
+//    // valhH should now equal the percentage of the 
+//    var valH = sldH.value;
+//    var budgetLeftOver = calcPerc(total,(total-budget));
+//    //var valV = sldV.value;
+//
+//    // calculate the percentage to pass an absolute length value 
+//    // to the clip property and determine the static value
+//    var leftVal = calcPerc(img.width, valH);
+//    var clipVal = getClipVal(leftVal);
+//
+//    // set the images' right offset clip accordingly
+//    img.style.clip = clipVal;
+//}
 
-    // calculate the percentage to pass an absolute length value 
-    // to the clip property and determine the static value
-    var leftVal = calcPerc(img.width, valH);
-    //var topVal = -1 * calcPerc(img.height, valV);
-    var clipVal = getClipVal(leftVal);
-
-    // set the images' right offset clip accordingly
-    img.style.clip = clipVal;
-}
-
-function calcPerc(range, val) {
-    return range / 100 * val;
+function calcPerc(total, budget) {
+    var percentage = 100- (budget/total*100)
+    return percentage;
 }
 
 function getClipVal(left) {
