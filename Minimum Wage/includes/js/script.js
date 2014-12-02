fe7b6bbca93d4d24f93ee3b27801a6249e78de96
ashirazi3 <ashirazi@gatech.edu>
2014-12-02 00:34:33 -0500

    fixed size issue, redoing calculations

--- Minimum Wage/includes/js/script.js
+++ Minimum Wage/includes/js/script.js
@@ -7,69 +7,69 @@ $(document).ready(function(){
         $("#dollarRow").prepend('<img id="dollar'+ i +'"" src="includes/images/dollar/dollar.jpg" style="width:10%"/>');
     }
     
-	$("#calculate").on('click', function(){
-		total = parseInt($("#salary").val());
-                console.log(total);
-                // Right after this total becomes null
-	$("form#expenseForm input[type=number]").each(function(){
-		if($(this).val()!==''){
-				total = parseInt(total) -     
-                        parseInt($(this).val())*parseInt($("#"+$(this).attr('id').concat("rate")).val());
-        }
-        $("#minimum").show();
-        $('#map').usmap({
-        stateStyles: {fill: '#E0E0E0'},
-        stateHoverStyles:{fill: '#203D6C'},      
-        click: function(event, data){
-            $("#map>svg>path").each(function(){
-                $(this).css('fill', ''); 
-            });
-            $('#' + data.name).css('fill', '#203D6C');
-            if(data.name==='CA'){
-                wage=9;
-            }else{
-                wage=7.25;
-            }              
-            $('html,body').animate({scrollTop: $('#expenses').offset().top}, 750);
-    
-            $('#map').usmap({stateHoverAnimation: 100});
-        }
-    });
-	});
-	if(total<0){
-		$("#number").text('-$' + Math.abs(total));
+//     $("#calculate").on('click', function(){
+//       total = parseInt($("#salary").val());
+//       console.log(total);
+//                 // Right after this total becomes null
+//                 $("form#expenseForm input[type=number]").each(function(){
+//                   if($(this).val()!==''){
+//                     total = parseInt(total) -     
+//                     parseInt($(this).val())*parseInt($("#"+$(this).attr('id').concat("rate")).val());
+//                 }
+//                 $("#minimum").show();
+//                 $('#map').usmap({
+//                     stateStyles: {fill: '#E0E0E0'},
+//                     stateHoverStyles:{fill: '#203D6C'},      
+//                     click: function(event, data){
+//                         $("#map>svg>path").each(function(){
+//                             $(this).css('fill', ''); 
+//                         });
+//                         $('#' + data.name).css('fill', '#203D6C');
+//                         if(data.name==='CA'){
+//                             wage=9;
+//                         }else{
+//                             wage=7.25;
+//                         }              
+//                         $('html,body').animate({scrollTop: $('#expenses').offset().top}, 750);
+                        
+//                         $('#map').usmap({stateHoverAnimation: 100});
+//                     }
+//                 });
+//             });
+// if(total<0){
+//   $("#number").text('-$' + Math.abs(total));
 
-	}else{
-		$("#number").text('$' + total);
-        
-	}
+// }else{
+//   $("#number").text('$' + total);
+  
+// }
 
-    var img = document.getElementById('image'), 
-        budgetLeftOver = null,
-        clipVal = null,
-        overBudgetPercentage = null,
-        starting = parseInt(wage)*2040;
-        
-	if(total>0){
-		$("#number").css("color", "green");
-        document.getElementById('image').src="includes/images/dollar/dollar.jpg";
-        budgetLeftOver = calcPerc(starting,total);
-        clipVal = image.width*budgetLeftOver;
-        console.log(budgetLeftOver, starting, total, clipVal);
-        img.style.clip = getClipVal(clipVal);
-        $("#number").html("Remaining In Budget: $" + total); 
-        
-	}else{
-		$("#number").css("color", "red");
-        document.getElementById('image').src="includes/images/dollar/reddollar.png";
-        total = total*-1;
-        overBudgetPercentage = (((total+starting)/starting)-1);
-        console.log(overBudgetPercentage, total, starting);
-        clipVal = image.width*overBudgetPercentage;
-        img.style.clip = getClipVal2(clipVal);
-        $("#number").html("Over Budget By: $" + total); 
-	}
-	});
+// var img = document.getElementById('image'), 
+// budgetLeftOver = null,
+// clipVal = null,
+// overBudgetPercentage = null,
+// starting = parseInt(wage)*2040;
+
+// if(total>0){
+//   $("#number").css("color", "green");
+//   document.getElementById('image').src="includes/images/dollar/dollar.jpg";
+//   budgetLeftOver = calcPerc(starting,total);
+//   clipVal = image.width*budgetLeftOver;
+//   console.log(budgetLeftOver, starting, total, clipVal);
+//   img.style.clip = getClipVal(clipVal);
+//   $("#number").html("Remaining In Budget: $" + total); 
+  
+// }else{
+//   $("#number").css("color", "red");
+//   document.getElementById('image').src="includes/images/dollar/reddollar.png";
+//   total = total*-1;
+//   overBudgetPercentage = (((total+starting)/starting)-1);
+//   console.log(overBudgetPercentage, total, starting);
+//   clipVal = image.width*overBudgetPercentage;
+//   img.style.clip = getClipVal2(clipVal);
+//   $("#number").html("Over Budget By: $" + total); 
+// }
+// });
 });
 
 function calcPerc(total, budget) {
@@ -87,7 +87,7 @@ function getClipVal2(right) {
 
 function clickState(event, data){
     $("#map > svg > path").each(function(){
-            $(this).css('fill', '');
+        $(this).css('fill', '');
     });
     $('#' + data.name).css('fill', 'pink');
 }
